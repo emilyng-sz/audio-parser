c316a3ebcdc2d61d112ebda815c1ec699a520f71
emilyng-sz <emilyng.sz831@gmail.com>
2026-02-10 01:26:39 +0800

    Add constants.py

---
+++ constants.py
@@ -0,0 +1,44 @@
+class VideoUploadDateFilter:
+    lastHour = 'EgQIARAB'
+    today = 'EgQIAhAB'
+    thisWeek = 'EgQIAxAB'
+    thisMonth = 'EgQIBBAB'
+    thisYear = 'EgQIBRAB'
+
+
+class VideoDurationFilter:
+    short = 'EgQQARgB'
+    medium = 'EgQQARgD'
+    long = 'EgQQARgC'
+
+
+class VideoSortOrder:
+    relevance = 'CAASAhAB'
+    uploadDate = 'CAISAhAB'
+    viewCount = 'CAMSAhAB'
+    rating = 'CAESAhAB'
+
+
+class VideoShortUploadDateFilter:
+    lastHour = 'EgYIARABGAE%253D'
+    today = 'EgYIAhABGAE%253D'
+    thisWeek = 'EgYIAxABGAE%253D'
+    thisMonth = 'EgYIBBABGAE%253D'
+    thisYear = 'EgYIBRABGAE%253D'
+
+
+class VideoMediumUploadDateFilter:
+    lastHour = 'EgYIARABGANCAggB'
+    today = 'EgYIAhABGANCAggB'
+    thisWeek = 'EgYIAxABGANCAggB'
+    thisMonth = 'EgYIBBABGANCAggB'
+    thisYear = 'EgYIBRABGANCAggB'
+
+
+class VideoUploadDateSortedDurationFilter:
+    short = 'CAISBBABGAFCAggB'
+    medium = 'CAISBBABGANCAggB'
+
+
+class OtherFilter:
+    exactSpelling = 'QgIIAQ%253D%253D'
